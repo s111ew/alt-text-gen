0abb62ad6ac3b314658f967695b6f0598074f42c
s111ew <167631312+s111ew@users.noreply.github.com>
2024-11-08 16:56:10 +0000

    Initialisation function for package

--- src/index.js
+++ src/index.js
@@ -5,3 +5,26 @@ const path = require("path");
 const { loadConfig, validateConfig } = require("./config");
 const { generateAltText, setupOpenAI } = require("./altTextGenerator");
 const { writeMarkdownFile } = require("./markdownWriter");
+
+async function init() {
+  const configPath = path.resolve(
+    __dirname,
+    "../config/alt-text-gen.config.js"
+  );
+
+  if (!fs.existsSync(configPath)) {
+    const defaultConfig = `
+      module.exports = {
+        imagePath: "./images",
+        markdownOutputPath: "./output.md",
+        ignoreImages: ["logo"],
+        apiKey: "YOUR_API_KEY_HERE",
+        additionalParams: "style=''"
+      };
+    `;
+    await fs.writeFile(configPath, defaultConfig);
+    console.log("Config file created at", configPath);
+  } else {
+    console.log("Config file already exists at", configPath);
+  }
+}
